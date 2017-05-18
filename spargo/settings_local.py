dd69fae43f762b68a68d95d718f1e1da6b1bede6
Muhamad Angga <muh.angga06@gmail.com>
2017-05-19 01:28:23 +0700

    update

--- spargo/settings_local.py
+++ spargo/settings_local.py
@@ -19,26 +19,4 @@ DATABASES = {
     }
 }
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
-        'APP_DIRS': False,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ],
-        },
-    },
-]
-
 RAVEN_CONFIG = {}
-
-HOST = "http://192.168.0.11:8000"
