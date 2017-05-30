fbea6e7575d8e86ad81093dae2038829d12c2b6b
Muhamad Angga <muh.angga06@gmail.com>
2017-05-30 23:05:00 +0700

    remove pyc

--- fabfiles/deploy.py
+++ fabfiles/deploy.py
@@ -53,20 +53,20 @@ def create_deployment_key():
 
 def deploy_project(target_dir='spargo'):
     """ Deploys a project for the first time """
-    setup_env(target_dir)
+    # setup_env(target_dir)
 
-    if exists("%s" % env.PROJECT_PATH):
-        abort('Project already deployed. Run "fab update_project -H <host>" instead')
+    # if exists("%s" % env.PROJECT_PATH):
+    #     abort('Project already deployed. Run "fab update_project -H <host>" instead')
 
-    setup_os()
-    require.files.directory(env.PROJECT_PATH, use_sudo=True,
-                            owner='www-data', group='www-data')
+    # setup_os()
+    # require.files.directory(env.PROJECT_PATH, use_sudo=True,
+    #                         owner='www-data', group='www-data')
     update_source_code()
-    setup_virtualenv()
-    log_dir = os.path.join(env.PROJECT_PATH, 'logs')
-    require.files.directory(log_dir, use_sudo=True,
-                            owner='www-data', group='www-data')
-    setup_webserver()
+    # setup_virtualenv()
+    # log_dir = os.path.join(env.PROJECT_PATH, 'logs')
+    #require.files.directory(log_dir, use_sudo=True,
+    #                        owner='www-data', group='www-data')
+    #setup_webserver()
 
     # TODO: Create DB is broken
     # create_db()
@@ -79,12 +79,12 @@ def deploy_project(target_dir='spargo'):
     # }
     # sudo('mv %ssettings_local.py.tpl %s/%s/settings_local.py' %
         # (env.HOME_PATH, env.SRC_PATH, env.PROJECT_NAME))
-    install_project_requirements()
-    install_front_end_requirements()
-    collect_static()
-    compress_static()
-    sudo('chown -R www-data.www-data %s' % env.PROJECT_PATH)
-    migrate()
+    #install_project_requirements()
+    #install_front_end_requirements()
+    #collect_static()
+    #compress_static()
+    #sudo('chown -R www-data.www-data %s' % env.PROJECT_PATH)
+    #migrate()
 
 
 def check_django_shell():
